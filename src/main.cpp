30e05c4ad1967ffeaec9a4b88241063b1f7d7f27
Yatomi <yatomi85384@gmail.com>
2026-06-11 23:30:20 +0900

    ESP32 first boot test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
+    Serial.println("Hello CanSat!");
 }
 
-void loop() {
-    Serial.println("Hello CanSat!");
+void loop()
+{
+    Serial.println("ESP32 OK");
     delay(1000);
 }
